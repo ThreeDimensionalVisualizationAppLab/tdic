c88ac0e11270b18c16c851ebe5dec4f04beda441
MOSUMA <65025326+ThreeDimensionalVisualizationAppLab@users.noreply.github.com>
2022-07-22 23:19:05 +0900

    dev_card

--- TDIC/client-app/src/features/Articles/dashboard/ArticleList.tsx
+++ TDIC/client-app/src/features/Articles/dashboard/ArticleList.tsx
@@ -1,7 +1,9 @@
 import { observer } from "mobx-react-lite";
-import { Row } from "react-bootstrap";
+import { Fragment } from "react";
+import { Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useStore } from "../../../app/stores/store";
+import "./CardStyle.css"
 
 export default observer( function ArticleList() {
     const {userStore: {user}} = useStore();
@@ -11,26 +13,22 @@ export default observer( function ArticleList() {
     return (
         <>
         <Row>
-            { 
-                Array.from(articleRegistry.values()).map(x=>(                    
 
-                    <div key={x.id_article} className="col-sm-6 col-md-4 col-xl-3 mb-3">
-                        <div>
+
+            { 
+                Array.from(articleRegistry.values()).map(x=>(
+                    <Col>
+                        <Card key={x.id_article} style={{ width: '24rem',  height: '30rem'}} className="article-dashboard-card" >
                             <Link to={`/article/${x.id_article}`}>
-                                {
-                                    <img className="img-thumbnail mb-3" src={process.env.REACT_APP_API_URL + `/attachmentfiles/file/${x.id_attachment_for_eye_catch}`} alt="" width="480" height="270" loading="lazy"></img>
-                                }
-                                <h3 className="h5 mb-1">{x.title}</h3>
+                                <Card.Img variant="top" src={process.env.REACT_APP_API_URL + `/attachmentfiles/file/${x.id_attachment_for_eye_catch}`} />
                             </Link>
-                            <p  className="text-muted">{x.short_description}</p>
-                            { user && 
-                                <>
-                                    <Link className="btn btn-outline-primary" to={`/articleedit/${x.id_article}`}>Edit</Link>
-                                    <p>{x.status}</p>
-                                </>
-                             }
-                        </div>
-                    </div>
+                            <Card.Body>
+                                <Card.Title>{x.title}</Card.Title>
+                                { user && <Link to={`/articleedit/${x.id_article}`}>Edit</Link> }
+                                <Card.Text>{x.short_description}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
             )) }
 
         </Row>
